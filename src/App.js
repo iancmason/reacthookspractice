50e49833a0f33e7bf1983b83dec39af87b093f43
Ian Mason <iancmason@users.noreply.github.com>
2023-08-31 14:09:26 -0500

    add toggles

--- src/App.js
+++ src/App.js
@@ -10,10 +10,30 @@ import "./App.css";
 function App() {
   const [toDoList, setToDoList] = useState(data);
 
+  const handleToggle = (id) => {
+    let mapped = toDoList.map((task) => {
+      return task.id == id
+        ? { ...task, complete: !task.complete }
+        : { ...task };
+    });
+    setToDoList(mapped);
+  };
+
+  const handleFilter = () => {
+    let filtered = toDoList.filter((task) => {
+      return !task.complete;
+    });
+    setToDoList(filtered);
+  };
+
   return (
     <div className="App">
       <Header />
-      <ToDoList toDoList={toDoList} setToDoList={setToDoList} />
+      <ToDoList
+        toDoList={toDoList}
+        handleToggle={handleToggle}
+        handleFilter={handleFilter}
+      />
     </div>
   );
 }
